a8c11e4f3929b13d05cf96499097c6bb119078c7
airrinaaa <i.rychok@ukma.edu.ua>
2024-03-13 16:28:08 +0200

    add checking if no more then 1000 lines entered

--- Main.java
+++ Main.java
@@ -1,28 +1,36 @@
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, List<Integer>> keyValueMap = new HashMap<>();
-        while (scanner.hasNextLine()){
+        int lineCount = 0; // Лічильник рядків
+
+        while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            if (line.isEmpty()){
-                break;//if an empty line entered then stop
+            if (line.isEmpty()) {
+                break; // Якщо введено порожній рядок - зупиняємо читання
             }
+
+            lineCount++; // Збільшуємо лічильник рядків
+
             String[] parts = line.split(" ");
             String key = parts[0];
             int value = Integer.parseInt(parts[1]);
+
             keyValueMap.compute(key, (k, v) -> (v == null) ? new ArrayList<>() : v).add(value);
+        }
 
-
-
+        if (lineCount > 1000) {
+            System.out.println("Перевищено максимальну кількість рядків (1000).");
+        } else {
+            for (String key : keyValueMap.keySet()) {
+                double average = computeAverage(keyValueMap.get(key));
+                System.out.println(key + " " + average);
+            }
         }
     }
 
-    /**
-     * method which calculates average value for each key
-     * @param values
-     * @return
-     */
     private static double computeAverage(List<Integer> values) {
         if (values == null || values.isEmpty()) {
             return 0.0;
@@ -35,7 +43,4 @@ public class Main {
 
         return (double) sum / values.size();
     }
-
 }
-
-
