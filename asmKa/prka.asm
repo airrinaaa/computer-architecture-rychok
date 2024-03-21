86a90ae4b594d7fc4c53564de634f71e81b27bc3
airrinaaa <161383605+airrinaaa@users.noreply.github.com>
2024-03-21 21:10:34 +0200

    dividing in 2 massives keys and values

--- asmKa/prka.asm
+++ asmKa/prka.asm
@@ -1,77 +1,59 @@
 .model small
 .stack 100h
-
 .data
-    buffer db 81 dup(?)  ; Збільшуємо розмір буфера на 1 для завершуючого нуль-символу
-    lineCount dw 0        ; Лічильник кількості стрічок
-    outputCount dw 0      ; Лічильник виведених стрічок
+buffer db 81 dup(?)
+lineCount dw 0
+outputCount dw 0
+keys db 81 dup(?)
+values db 81 dup(?)
+keys_size dw 81
+values_size dw 81
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
-
-    mov bx, 0  ; Обнулення лічильника стрічок
+    mov bx, 0
 
 read_loop:
-    ; Перевірка, чи виведено вже 1000 рядків
-    mov ax, [outputCount]
-    cmp ax, 1000
-    je ending
+    mov ah, 3Fh
+    lea dx, buffer
+    mov cx, 80h
+    int 21h
+    or ax, ax
+    jz ending
+    mov si, offset buffer
+    mov di, offset keys
 
-    mov ah, 3Fh       
-    mov bx, 0        
-    lea dx, buffer   
-    mov cx, 80h      
-    int 21h         
+find_space:
+    lodsb
+    cmp al, ' '
+    je got_space
+    stosb
+    jmp find_space
 
-    cmp ax, cx
-    jae checking_eof    
-    mov cx, ax       
+got_space:
+    mov byte ptr [di], '$'
+    inc si
+    mov di, offset values
 
-    inc word ptr [lineCount]  ; Збільшення лічильника стрічок
+store_values:
+    lodsb
+    stosb
+    cmp al, '$'
+    je print_keys_and_values
+    jmp store_values
 
-    ; Перевірка, чи досягнуто 1000 стрічок
-    mov ax, [lineCount]
-    cmp ax, 1000
-    je print_loop
-
-    ; Виведення стрічки на екран
-    mov ah, 02h      
-    mov si, 0        
-
-print_loop:
-    mov dl, buffer[si] 
-    int 21h           
-    inc si            
-    cmp dl, 0          ; Перевірка, чи досягли ми завершуючого нуль-символу
-    je reset_buffer    ; Якщо так, очищуємо буфер та переходимо до нової ітерації зчитування
-    loop print_loop   
-
-    ; Збільшення лічильника виведених стрічок
-    inc word ptr [outputCount]
-
-    jmp read_loop
-
-checking_eof:
-    ; Перевірка на кінець файлу
-    mov ah, 3Eh       ; Функція DOS для перевірки EOF
-    mov bx, 0     
-    int 21h           ;DOS-переривання
-
-    ; Якщо вказівник EOF не дорівнює 128 (0x80), - що файл закінчився
-    cmp ax, 80h
-    jne ending   ;EOF = завершуємо програму
-
-    jmp main
-
-reset_buffer:
-    mov byte ptr [buffer], 0  ; Очищення першого символу буфера
+print_keys_and_values:
+    mov ah, 09h
+    lea dx, keys
+    int 21h
+    lea dx, values
+    int 21h
     jmp read_loop
 
 ending:
     mov ax, 4C00h
     int 21h
-
 main endp
 end main
