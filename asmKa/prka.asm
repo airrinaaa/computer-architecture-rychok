527c9a284d96e1718630d5fc9a7e83a0dcc72b97
airrinaaa <161383605+airrinaaa@users.noreply.github.com>
2024-03-21 16:13:41 +0200

    reading and printing all the lines in the file test.in without trash

--- asmKa/prka.asm
+++ asmKa/prka.asm
@@ -2,12 +2,22 @@
 .stack 100h
 
 .data
-    buffer db 80h dup(?)
+    buffer db 81 dup(?)  ; Збільшуємо розмір буфера на 1 для завершуючого нуль-символу
+    lineCount dw 0        ; Лічильник кількості стрічок
+    outputCount dw 0      ; Лічильник виведених стрічок
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
+
+    mov bx, 0  ; Обнулення лічильника стрічок
+
+read_loop:
+    ; Перевірка, чи виведено вже 1000 рядків
+    mov ax, [outputCount]
+    cmp ax, 1000
+    je ending
 
     mov ah, 3Fh       
     mov bx, 0        
@@ -19,15 +29,29 @@ main proc
     jae checking_eof    
     mov cx, ax       
 
-display:
+    inc word ptr [lineCount]  ; Збільшення лічильника стрічок
+
+    ; Перевірка, чи досягнуто 1000 стрічок
+    mov ax, [lineCount]
+    cmp ax, 1000
+    je print_loop
+
+    ; Виведення стрічки на екран
     mov ah, 02h      
     mov si, 0        
 
 print_loop:
-    mov dl, buffer[si] ; Вставляємо символ для виведення
-    int 21h           ; Виводимо символ
-    inc si            ; Наступний символ
-    loop print_loop   ; Повторюємо, поки CX != 0
+    mov dl, buffer[si] 
+    int 21h           
+    inc si            
+    cmp dl, 0          ; Перевірка, чи досягли ми завершуючого нуль-символу
+    je reset_buffer    ; Якщо так, очищуємо буфер та переходимо до нової ітерації зчитування
+    loop print_loop   
+
+    ; Збільшення лічильника виведених стрічок
+    inc word ptr [outputCount]
+
+    jmp read_loop
 
 checking_eof:
     ; Перевірка на кінець файлу
@@ -35,42 +59,19 @@ checking_eof:
     mov bx, 0     
     int 21h           ;DOS-переривання
 
-    ; Якщо вказівник EOF не дорівнює 128 (0x80) - файл закінчився
+    ; Якщо вказівник EOF не дорівнює 128 (0x80), - що файл закінчився
     cmp ax, 80h
-    jne ending   ; Якщо EOF, завершуємо програму
+    jne ending   ;EOF = завершуємо програму
 
-    
     jmp main
 
-calculate_average:
-    ; Обчислення суми та кількості
-    mov cx, 80h
-    lea si, buffer
-sumLoop:
-    mov al, [si]
-    add sum, ax
-    inc count
-    inc si
-    loop sumLoop
+reset_buffer:
+    mov byte ptr [buffer], 0  ; Очищення першого символу буфера
+    jmp read_loop
 
-    ; Обчислення середнього арифметичного
-    mov ax, sum
-    cwd ; 
-    div count
-
-    ; Виведення середнього арифметичного
-    mov cx, 11
-    lea si, buffer
-outLoop:
-    mov dl, [si]
-    mov ah, 02h
+ending:
+    mov ax, 4C00h
     int 21h
 
-    inc si
-    loop outLoop
-
-    ; Завершення програми
-    mov ax, 4c00h
-    int 21h
 main endp
 end main
